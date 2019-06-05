f09323f6b6348bf739e26cab6ea8bfc9618a6e28
Mihai Martalogu <mihai@martalogu.com>
2019-06-05 19:35:09 +0200

    Updates

---
+++ precache-manifest.ae6af831f7b1a180041df0635a3dbf70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f331346043885ab18ac6a97c907009",
+    "url": "/gmail-to-telegram/index.html"
+  },
+  {
+    "revision": "b3a83727eda37466e724",
+    "url": "/gmail-to-telegram/static/css/main.9a01b0d3.chunk.css"
+  },
+  {
+    "revision": "6ba769478098332af192",
+    "url": "/gmail-to-telegram/static/js/2.b4f65d19.chunk.js"
+  },
+  {
+    "revision": "b3a83727eda37466e724",
+    "url": "/gmail-to-telegram/static/js/main.82d6b23d.chunk.js"
+  },
+  {
+    "revision": "4f655e3264b9b2bac8e8",
+    "url": "/gmail-to-telegram/static/js/runtime~main.11c7458c.js"
+  }
+]);
